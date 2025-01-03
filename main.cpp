7f2485f343b6ec6c93270041e0eaffdbe7b13c22
Muneeb Haroon <153209688+MUNEEB68@users.noreply.github.com>
2025-01-04 02:15:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "photeditor.h"
+#include "ImageCraft.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    photeditor w;
+    ImageCraft w;
     w.show();
     return a.exec();
 }
+
+
